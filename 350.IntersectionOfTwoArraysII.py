6ecc0a3cb961dea99d1c8065fb5210eec9714a86
Ming Yao <yao9208@foxmail.com>
2016-05-20 23:23:21 -0700

    349

---
+++ 350.IntersectionOfTwoArraysII.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort()
+        nums2.sort()
+        ptr1, ptr2 = 0, 0
+        result = []
+        while ptr1<len(nums1) and ptr2<len(nums2):
+            if nums1[ptr1]==nums2[ptr2]:
+                result.append(nums1[ptr1])
+                ptr1 += 1
+                ptr2 += 1
+            elif nums1[ptr1]<nums2[ptr2]:
+                ptr1 += 1
+            else:
+                ptr2 += 1
+        return result
